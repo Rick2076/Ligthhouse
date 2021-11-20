bfc6464347b97e593ed8ab2d2fc8d64fe4add24a
Rick_2077 <90279916+Rick2076@users.noreply.github.com>
2021-11-20 01:17:21 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,22 +3,77 @@ import sys
 import psycopg
 import datetime
 
+def writeFilePostgres(tableName, current_date, content):
+
+    # create a directory for the date stamp if it doesn't exist.
+    try:
+        absolute_path = os.path.dirname(__file__) + "/data/postgres/" + tableName
+        path_table_write = os.path.join(absolute_path, current_date)
+        os.mkdir(path_table_write)
+    except:
+        print("Updating previously existant postgres backup!")
+    
+    # open and create the folders to write the pseudo-CSV
+    tableCSV = open("./data/postgres/" + tableName + "/" + current_date + "/" + tableName + ".csv", "a")
+
+    for line in content:
+        tableCSV.write(str(line) + '\n')
+
+    tableCSV.close()
+
 def readFromPostgres(current_date):
     # connecting to the SGBD
-    with psycopg.connect("dbname=postgres user=postgres password=postgres") as conn:
+    with psycopg.connect("dbname=northwind user=postgres password=postgres") as conn:
         with conn.cursor() as cur:
-            return
+            cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
+            table_names = cur.fetchall()
+            # print(table_names)
+
+            for table in table_names:
+                table = table[0]
+
+                # create directory for current table.
+                try:
+                    absolute_path = os.path.dirname(__file__) + "/data/postgres/"
+                    path_table_write = os.path.join(absolute_path, table)
+                    os.mkdir(path_table_write)
+                except:
+                    pass
+
+                cur.execute("select * from " + table)
+                tuples = cur.fetchall()
+
+                writeFilePostgres(table, current_date,tuples)
+                
     
 def readFromCSV(current_date):
     # connecting the CSV file to the CSV folder
     try:
+        # create a directory for the csv with the date stamp.
+        try:
+            absolute_path = os.path.dirname(__file__) + "/data/csv/"
+            path_csv_write = os.path.join(absolute_path, current_date)
+            os.mkdir(path_csv_write)
+        except:
+            print("Updating previously existant CSV!")
+        
+        # open and create the folders to write the csv
         order_details = open("./data/order_details.csv", "r")
-        order_details_date = open("./data/" + current_date + ".csv", "w")
-        while(order_details.)
+        order_details_write = open("./data/csv/" + current_date + "/order_details.csv", "a")
+
+        # we copy the file line per line until we reach its end
+        try:
+            while(True):
+                order_details_tuple = order_details.readline()
+                order_details_write.write(order_details_tuple)
+
+        except:
+            print("Finished writing the CSV file.")
 
         order_details.close()
+        order_details_write.close()
     except:
-        print("Error on trying to open 'oder_detais.csv'!")
+        print("Error on trying to open 'order_detais.csv'!")
 
 
 def main():
@@ -36,25 +91,13 @@ def main():
         print("Data directory already existant, skipping data creation step.")
 
     time = datetime.datetime.now()
-    current_date = time.year + "-" + time.month + "-" + time.day
+    current_date = str(time.year) + "-" + str(time.month) + "-" + str(time.day)
 
     # step 1
-    # readFromPostgres(current_date)
-    readFromCSV(current_date)
+    readFromPostgres(current_date)
+    # readFromCSV(current_date)
 
     # step 2
     # pushToDatabase()
-
-#    try:
-#        cur.execute("SELECT * FROM customers")
-#        cur.fetchall()
-#    cur.execute("SELECT * FROM orders limit 1")
-
-#    for record in cur: 
-#        print(record)
-
-#         # Make the changes to the database persistent
-#         conn.commit
-        
     
 main()
